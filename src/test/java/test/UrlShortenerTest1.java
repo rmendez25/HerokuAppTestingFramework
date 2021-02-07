f632afdc4d9d5e034addf141967f08b378fafffe
rmendez25 <rmendezfamilia@gmail.com>
2021-02-07 19:15:54 -0400

    Adding a new test

--- src/test/java/test/UrlShortenerTest1.java
+++ src/test/java/test/UrlShortenerTest1.java
@@ -4,14 +4,12 @@ import org.testng.annotations.Test;
 import page.UrlShortenerPage;
 import utils.BaseTest;
 
-public class UrlShortenerTest extends BaseTest {
-
+public class UrlShortenerTest1 extends BaseTest {
     UrlShortenerPage url;
 
     @Test
-    public void testUrlShortener(){
+    public void testUrlShortener1() {
         url = new UrlShortenerPage(getDriver());
-        url.testUrlShortener("https://www.google.com/");
+        url.testUrlShortener("https://www.youtube.com/");
     }
-
 }
